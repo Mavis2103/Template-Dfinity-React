ab54f884f54f12ac59bcf4a6e10a456f3eb701bb
Mavis2103 <taquanvku@gmail.com>
2021-10-17 17:25:43 +0700

    init

--- src/frontend_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { frontend } from "../../declarations/frontend";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with frontend actor, calling the greet method
-  const greeting = await frontend.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
